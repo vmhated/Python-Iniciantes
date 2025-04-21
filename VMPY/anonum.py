d545147defe6f6d0bfdc7c0709e6a2e3ff90ae22
Victor Manoel <vctrzmk@gmail.com>
2025-04-21 12:26:59 -0300

    Update anonum.py

--- VMPY/anonum.py
+++ VMPY/anonum.py
@@ -4,12 +4,3 @@ if ano/4 or ano/400 and not ano/100:
 	print("Ano é bissexto")
 else:
 	print("Ano é normal")
-#Positivo,negativo ou zero#
-N1 = int(input("Digite o número:"))
-if N1>0:
-	print("Número é positivo")
-elif N1<0:
-	print("Número é negativo")
-else:
-	print("Número é zero")
-	
